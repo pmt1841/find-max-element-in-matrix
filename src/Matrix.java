9c9f9da8e81cc16086dc1f70e9b0edc80ede620f
pmt1841 <phamminhtien1841@gmail.com>
2025-11-13 23:19:46 +0700

    Update src code

--- src/Matrix.java
+++ src/Matrix.java
@@ -13,22 +13,37 @@ public class Matrix {
         int columns = sc.nextInt();
 
         int[][] matrix = new int[rows][columns];
+        addElement(matrix);
+        System.out.println("Matrix: " + Arrays.deepToString(matrix));
+
+        System.out.println("Maximum element in matrix: " + findMaxElement(matrix));
+
+    }
+
+    public static void addElement(int[][] matrix) {
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < columns; c++) {
                 System.out.printf("Enter the elements [%d][%d] of the matrix: ", r, c);
                 matrix[r][c] = sc.nextInt();
             }
         }
-        System.out.println("Matrix: " + Arrays.deepToString(matrix));
+    }
 
+    public static int findMaxElement(int[][] matrix) {
+        int columns = matrix[0].length;
         int max = matrix[0][0];
-        for (int r = 0; r < rows; r++) {
+
+        for (int[] element : matrix) {
             for (int c = 0; c < columns; c++) {
-                if (matrix[r][c] > max) {
-                    max = matrix[r][c];
+                if (element[c] > max) {
+                    max = element[c];
                 }
             }
         }
-        System.out.println("Maximum element in matrix: " + max);
+        return max;
     }
 }
